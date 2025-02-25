2d58563f441bb8d71b77687a8b07cee30c5446ce
diegotero <diegotero.cba@gmail.com>
2025-02-25 10:36:34 -0300

    Initial commit from your app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d609ac04deb9f29f"; }
+/******/ 		__webpack_require__.h = function() { return "4a3d35fcfa86dc68"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- app/services/page.jsx
+++ app/services/page.jsx
@@ -1,38 +1,38 @@
 import PageTemplate from '@/components/layout/page-template'
-    import { getServices } from '@/lib/services'
-    import Image from 'next/image'
-    
-    export default async function ServicesPage() {
-      const services = await getServices()
-    
-      return (
-        <PageTemplate title="Our services, what we know best">
-          
-            {services.map((service) => (
-              
-                
-                  {service.title}
-                
-                
-                  {service.description}
-                  
-                    {service.longDescription}
-                  
-                
-                
-                  <Image
-                    src={service.image.url}
-                    alt={service.title}
-                    width={500}
-                    height={300}
-                  />
-                
-                
-                  {service.buttonText}
-                
-              
-            ))}
-          
-        </PageTemplate>
-      )
-    }
+import { getServices } from '@/lib/services'
+import Image from 'next/image'
+
+export default async function ServicesPage() {
+  const services = await getServices()
+
+  return (
+    <PageTemplate title="Our services, what we know best">
+      <div>
+        {services.map((service) => (
+          <div key={service.id}>
+            <div>
+              {service.title}
+            </div>
+            <div>
+              {service.description}
+              <div>
+                {service.longDescription}
+              </div>
+            </div>
+            <div>
+              <Image
+                src={service.image.url}
+                alt={service.title}
+                width={500}
+                height={300}
+              />
+            </div>
+            <div>
+              {service.buttonText}
+            </div>
+          </div>
+        ))}
+      </div>
+    </PageTemplate>
+  )
+}
