20d75c5d2b23eb014bcceba919bf2116ba52469c
diegotero <diegotero.cba@gmail.com>
2025-02-25 10:39:12 -0300

    Initial commit from your app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4a3d35fcfa86dc68"; }
+/******/ 		__webpack_require__.h = function() { return "59529027013a088c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/layout/page-template.jsx
+++ components/layout/page-template.jsx
@@ -1,26 +1,26 @@
 import { Header } from '@/components/layout/header'
-    import { Footer } from '@/components/layout/footer'
-    import { Button } from '@/components/ui/button'
+import { Footer } from '@/components/layout/footer'
+import { Button } from '@/components/ui/button'
+
+export default function PageTemplate({ title, children }) {
+  return (
     
-    export default function PageTemplate({ title, children }) {
-      return (
+      
+        <Header />
         
           
-            <Header />
+            <h1 className="text-3xl font-bold mb-4">{title}</h1>
             
-              
-                <h1 className="text-3xl font-bold mb-4">{title}</h1>
-                
-                  <Button >Button 1</Button>
-                  <Button >Button 2</Button>
-                
-              
+              <Button>Button 1</Button>
+              <Button>Button 2</Button>
             
-            
-              {children}
-            
-            <Footer />
           
         
-      )
-    }
+        
+          {children}
+        
+        <Footer />
+      
+    
+  )
+}
